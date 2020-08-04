5973361476b7a447f43540a6afbd85f19710e4ed
Cheryl <cheryl.nqj@gmail.com>
2020-08-04 11:54:14 +0800

    Change alert info

--- src/components/Events/EventsAlert.js
+++ src/components/Events/EventsAlert.js
@@ -8,12 +8,20 @@ const EventsAlert = props => {
 
   return (
     <Alert color="primary" isOpen={visible} toggle={onDismiss}>
-      <div className="text-center">
+      {/* <div className="text-center">
       Due to the COVID-19 outbreak, all IGs, GUIs, Houses and other USP groups will not be able to hold any face-to-face activities, and bookings for such purposes will not be considered valid during this time. Refer {' '}
         <a href="https://emergency.nus.edu.sg/" className="alert-link">
           here
         </a>
       {' '} for more information.
+      </div> */}
+      <div className="text-center">
+      USC is hard at work to adhere to NUS' policies during the COVID-19 outbreak. All bookings made during this time on the site will not be valid. We'll be right back! For more information,
+      {' '}
+        <a href="https://emergency.nus.edu.sg/" className="alert-link">
+          head here
+        </a>
+      {' '}.
       </div>
     </Alert>
   )
